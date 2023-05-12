2f4a31256a44894f09f04a236147076e88ebaf35
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-05-12 16:07:58 -0400

    Created animal prefabs.

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveForward : MonoBehaviour
 {
     // speed var
-    private float speed = 40.0f;
+    public float speed = 40.0f;
 
     // Start is called before the first frame update
     void Start()
