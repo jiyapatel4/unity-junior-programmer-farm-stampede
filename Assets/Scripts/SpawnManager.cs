c3ad6bbe7771660fdcde324e88fca9438ae587b1
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-05-14 17:11:17 -0400

    Updated SwanManager to make animals spawn independently
    - Instead of spawning after a button click the animals spawn indefinitely

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -11,28 +11,35 @@ public class SpawnManager : MonoBehaviour
     private int spawnRangeX = 10;
     private int spawnPosZ = 20;
 
+    // spawn delay vars
+    private int startDelay = 2;
+    private float spawnDelay = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // call the method after 2 seconds every 1.5 seconds
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnDelay);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // when the S key is pressed
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // pick a random index value between 0 and and 3
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-
-            // create a new Vector3 object where the x value is generated randomly between a specific range
-            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-
-            // create instances of random animals at random locations in the game scene
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
             
-        }
         
+    }
+    
+    // method that will spawn random animals
+    void SpawnRandomAnimal()
+    {
+        // pick a random index value between 0 and and 3
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        // create a new Vector3 object where the x value is generated randomly between a specific range
+        Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        // create instances of random animals at random locations in the game scene
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
